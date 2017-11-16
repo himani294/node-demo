a84dae30e6fed20b91f38ff4849844498000e267
himani294 <33711351+himani294@users.noreply.github.com>
2017-11-16 12:18:37 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+//for post data
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+var data_controller = require('./DataController');
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/FetchAllData/', data_controller.FetchAllData);
+app.post('/FetchData/',data_controller.FetchData);
+app.get('/addData/',data_controller.addData);
+app.post('/deleteData/',data_controller.deleteData);
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
