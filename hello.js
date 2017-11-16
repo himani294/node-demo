96951667b260ddb09bfae6fdad3dac3bcb535697
himani294 <33711351+himani294@users.noreply.github.com>
2017-11-16 12:47:20 +0530

    Add files via upload

--- hello.js
+++ hello.js
@@ -15,5 +15,5 @@ app.get('/FetchAllData/', data_controller.FetchAllData);
 app.post('/FetchData/',data_controller.FetchData);
 app.get('/addData/',data_controller.addData);
 app.post('/deleteData/',data_controller.deleteData);
-
+app.post('/editData/',data_controller.editData);
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
